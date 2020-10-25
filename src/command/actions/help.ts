a5db92d16301e163637cd73b6bfc45c9f295fc61
Indimeco <18373729+Indimeco@users.noreply.github.com>
2020-10-26 08:43:52 +1100

    feat: add help for specific commands

--- src/command/actions/help.ts
+++ src/command/actions/help.ts
@@ -1,20 +1,33 @@
-import { map, pipe, prop, join, isEmpty } from "ramda";
-import { ActionItem, getActions } from "."
+import { map, pipe, prop, join, isEmpty, length } from "ramda";
+import { ActionItem, getActions, getAction } from "."
+
+const listCommands = () => pipe(
+    map(getCommandDetail),
+    join('\n')
+)(getActions());
+
+const getCommandDetail = (action: ActionItem) => `**${prop('command', action)}**: ${prop('desc', action)}`;
+const getCommandUsage = (action: ActionItem) => `\`${prop('usage', action)}\``;
 
 export const help: ActionItem = {
     command: 'help',
     desc: 'describe commands Triona can action',
     usage: 'help <command>',
     exec: (message, args) => {
-        console.log(args);
         if (isEmpty(args)) {
-            const allCommands = pipe(
-                map(
-                    (action: ActionItem) => `**${prop('command', action)}:** ${prop('desc', action)}`
-                ),
-                join('\n')
-            )(getActions());
-            message.channel.send(allCommands, { split: true })
+            message.channel.send(listCommands(), { split: true })
+            return;
         }
+
+        if (length(args) === 1) {
+            const action = getAction(args[0]);
+            if (action) {
+                message.channel.send(`${getCommandDetail(action)}\n${getCommandUsage(action)}`);
+                return;
+            }
+        }
+
+        message.channel.send('Command not found. Use `help` to list available commands.')
+        return;
     }
 }
